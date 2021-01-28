7cf58a4f345422546efd30b945f7f831ffb3e4dc
Luis Aveiro <git@luisaveiro.com>
2021-01-28 22:43:01 +0000

    BREAKING CHANGE: removed global WHITE & NC constants from config.

--- bin/config.sh
+++ bin/config.sh
@@ -2,8 +2,6 @@
 #
 # Define global constants.
 
-export readonly WHITE='\033[1;37m'
-export readonly NC='\033[0m'
 export readonly DOCKER_COMPOSE_FILE="${CURRENT_DIR}/../docker/docker-compose.yml"
 export readonly PROPERTIES_FILE="sonar-project.properties"
 export readonly SONARQUBE_DASHBOARD_URL="http://localhost:9000"
