0fa06801087e543095142d096547244fa6991e7b
Luis Aveiro <git@luisaveiro.com>
2021-01-28 22:20:05 +0000

    feat: created package manager

---
+++ bin/packages/packages.sh
@@ -0,0 +1,16 @@
+# shellcheck shell=bash
+#
+# Simple package manager to load third-party packages.
+
+readonly PACKAGES_DIR="${CURRENT_DIR}/packages"
+
+readonly PACKAGES=(
+  "ansi/ansi.sh"
+)
+
+for package in "${PACKAGES[@]}"
+do
+  # shellcheck source=/dev/null
+  source "${PACKAGES_DIR}/${package}"
+done
+
