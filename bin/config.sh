b2fa6b8e4c7b9d1809a24d0b2fb0a43559a50730
Luis Aveiro <git@luisaveiro.com>
2021-01-29 07:27:59 +0000

    feat: add PROJECT_DIR & TEMPLATE_DIR global constants

--- bin/config.sh
+++ bin/config.sh
@@ -2,8 +2,16 @@
 #
 # Define global constants.
 
-export readonly DOCKER_COMPOSE_FILE="${CURRENT_DIR}/../docker/docker-compose.yml"
+readonly PROJECT_DIR=$(dirname "${CURRENT_DIR}")
+
+# Docker compose
+export readonly DOCKER_COMPOSE_FILE="${PROJECT_DIR}/docker/docker-compose.yml"
+
+# SonarScanner template
+export readonly TEMPLATE_DIR="${PROJECT_DIR}/sonarscanner-templates"
 export readonly PROPERTIES_FILE="sonar-project.properties"
+
+# SonarQube
 export readonly SONARQUBE_DASHBOARD_URL="http://localhost:9000"
 export readonly SONARSCANNER_DOCKER_NETWORK="sonarqube-network"
 export readonly SONARSCANNER_DOCKER_IMAGE="sonarsource/sonar-scanner-cli:latest"
