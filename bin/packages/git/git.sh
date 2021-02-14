fe1c02b08cb9cd0bd20856f9f8e0161d23ac03a0
Luis Aveiro <git@luisaveiro.com>
2021-02-14 14:44:57 +0000

    feat: created git helper package

---
+++ bin/packages/git/git.sh
@@ -0,0 +1,36 @@
+# shellcheck shell=bash
+#
+# Package with helpful git commands.
+
+#######################################
+# Get the latest Git tag.
+#
+# Arguments:
+#   --dir
+#
+# Outputs:
+#   git latest tag.
+#######################################
+function git::latest_tag() {
+  local dir git_sub_command
+
+  while [ $# -gt 0 ]; do
+    if [[ $1 == *"--"* && $1 == *"="* ]]; then
+      local argument="${1/--/}"
+
+      IFS='=' read -ra parameter <<< "${argument}"
+
+      declare "${parameter[0]}"="${parameter[1]}"
+    fi
+
+    shift
+  done
+
+  if directory_exists "${dir}/.git"; then
+    git_sub_command="$(cd "${dir}" && git rev-list --tags --max-count=1)"
+
+    echo -e "$(cd "${dir}" && git describe --tags "$git_sub_command")"
+  else
+    echo "unreleased"
+  fi
+}
