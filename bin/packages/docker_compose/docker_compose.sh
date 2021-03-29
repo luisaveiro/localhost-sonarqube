a873d058dfc7f75b9bc921b2533ba3c39dcbaf34
Luis Aveiro <git@luisaveiro.com>
2021-03-29 06:20:24 +0100

    feat: set an alternate project name for docker compose.

--- bin/packages/docker_compose/docker_compose.sh
+++ bin/packages/docker_compose/docker_compose.sh
@@ -7,21 +7,25 @@
 #
 # Arguments:
 #   --file
+#   --project_name
 #   command
 #######################################
 function docker_compose::command() {
-  local file command
+  local arguments_list=("file" "project_name")
+  local file project_name command
 
   command=$*
 
   while [ $# -gt 0 ]; do
-    if [[ $1 == *"--file="* ]]; then
+    if [[ $1 == *"--"* && $1 == *"="* ]]; then
       local argument="${1/--/}"
-      command="${command/--${argument}/}"
 
       IFS='=' read -ra parameter <<< "${argument}"
 
-      declare "${parameter[0]}"="${parameter[1]}"
+      if [[ "${arguments_list[*]}" =~ ${parameter[0]} ]]; then
+        command="${command/--${argument}/}"
+        declare "${parameter[0]}"="${parameter[1]}"
+      fi
     fi
 
     shift
@@ -29,5 +33,8 @@ function docker_compose::command() {
 
   IFS=' ' read -ra command <<< "${command}"
 
-  docker-compose --file "${file}" "${command[@]}"
+  docker-compose \
+    --file "${file}" \
+    --project-name "${project_name}" \
+    "${command[@]}"
 }
