a853ac2237d4125d133c998da450abd88b003abe
Luis Carlos Aveiro <git@luisaveiro.com>
2023-02-14 19:10:54 +0000

    feat: add SONARQUBE_PORT config.

--- bin/config/sonarqube.sh
+++ bin/config/sonarqube.sh
@@ -3,8 +3,15 @@
 # Define Localhost SonarQube global SonarQube constants.
 
 #######################################
+# SonarQube Dashboard Port
+#######################################
+
+SONARQUBE_PORT="$(dotenv::get SONARQUBE_PORT --default=9000)"
+readonly SONARQUBE_PORT
+
+#######################################
 # SonarQube Dashboard URL
 #######################################
 
-readonly SONARQUBE_DASHBOARD_URL="http://localhost:9000"
+readonly SONARQUBE_DASHBOARD_URL="http://localhost:${SONARQUBE_PORT}"
 export SONARQUBE_DASHBOARD_URL
