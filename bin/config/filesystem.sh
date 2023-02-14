a42e0cfe4c347538982b8ab3acd3c3b4f257871d
Luis Carlos Aveiro <git@luisaveiro.com>
2023-02-14 17:33:28 +0000

    Revert "style: remove unnecessary spacing."
    This reverts commit 354833eb3e55a766e9b1f90376dd1fc571262209.

--- bin/config/filesystem.sh
+++ bin/config/filesystem.sh
@@ -6,7 +6,7 @@
 # Root Directory
 #######################################
 
-PROJECT_DIR="$(dirname "${BIN_DIR}")"
+PROJECT_DIR="$( dirname "${BIN_DIR}" )"
 readonly PROJECT_DIR
 export PROJECT_DIR
 
